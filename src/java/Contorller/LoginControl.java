6d0a05d60c208c40c9b9b7c3d18742c6f4a227a5
@yzdshahin <shahin234yazdani@gmail.com>
2020-09-30 17:26:52 +0330

    new changes of dao

--- src/java/Contorller/LoginControl.java
+++ src/java/Contorller/LoginControl.java
@@ -26,21 +26,34 @@ public class LoginControl extends HttpServlet {
         String father = request.getParameter("father");
         String birth = request.getParameter("birth");
         String gender = request.getParameter("gender");
-
-
-            PersonDAO.RegistoreNow(name, password,father,birth,gender);
+            PersonDAO p= new PersonDAO();
+            String userRegistered = p.RegistoreNow(name, password, father, birth, gender);
+            if(userRegistered.equals("SUCCESS"))   //On success, you can display a message to user on Home page
+         {
+            request.getRequestDispatcher("/Home.jsp").forward(request, response);
+         }
+         else   //On Failure, display a meaningful message to the User.
+         {
+            request.setAttribute("errMessage", userRegistered);
+            request.getRequestDispatcher("/Register.jsp").forward(request, response);
+         }
+         
+         
+        
+        
+         
           
             
-        if (PersonDAO.validateUser(name)){
-            if(Hash.MakeHash(password).equals(PersonDAO.validatePass(password))){
-            System.out.println("you're welcome");
-            RequestDispatcher requestDispatcher= request.getRequestDispatcher("SuccessPage.jsp");
-            requestDispatcher.forward(request,response);
-        }}else {
-            System.out.println("Try again");
-            RequestDispatcher requestDispatcher=request.getRequestDispatcher("ErrorPage.jsp");
-            requestDispatcher.forward(request,response);
-        }
+//        if (PersonDAO.validateUser(name)){
+//            if(Hash.MakeHash(password).equals(PersonDAO.validatePass(password))){
+//            System.out.println("you're welcome");
+//            RequestDispatcher requestDispatcher= request.getRequestDispatcher("SuccessPage.jsp");
+//            requestDispatcher.forward(request,response);
+//        }}else {
+//            System.out.println("Try again");
+//            RequestDispatcher requestDispatcher=request.getRequestDispatcher("ErrorPage.jsp");
+//            requestDispatcher.forward(request,response);
+//        }
 
     }
 
--- src/java/Model/DAO/PersonDAO.java
+++ src/java/Model/DAO/PersonDAO.java
@@ -7,7 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.security.NoSuchAlgorithmException;
-
+import Model.Entity.Person;
 
 public class PersonDAO {
     
@@ -51,22 +51,21 @@ public class PersonDAO {
         return validation;
     }
            
-                  public static boolean RegistoreNow(String NameUser,String PassUser,String FatherUser,String BirthUser,String GenderUser) throws SQLException {
-            boolean validation= false;
+                  public String RegistoreNow(String name,String password, String father,String birth,String gender) throws SQLException {
+               boolean validation= false;
             try{ ConnectionProvider connectioner2 =new ConnectionProvider();
-            PreparedStatement   preparedStatement = connectioner2.getConnection().prepareStatement("insert into table Employee where name=? and pass=? and father=? and birth=? and gender=?");
+            PreparedStatement   preparedStatement = connectioner2.getConnection().prepareStatement("insert into Employee where name=? and pass=? and father=? and birth=? and gender=?");
             preparedStatement.setString(1, "name");
             preparedStatement.setString(2, "password");
             preparedStatement.setString(3, "father");
             preparedStatement.setString(4, "birth");
             preparedStatement.setString(5, "gender");
 
-            ResultSet resultSet3= preparedStatement.executeQuery();
-            validation=resultSet3.next();
+            int resultSet3= preparedStatement.executeUpdate();
+              if (resultSet3 !=0) return "SUCCESS";
            }catch(SQLException e){
-               System.out.println("the user you entered is not registered");} 
-           
-        return validation;
+               e.printStackTrace();} 
+        return "Oops.. Something went wrong there..!";
     }
 
     private static boolean SQLException(SQLException e) {
